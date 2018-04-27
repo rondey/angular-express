45aa4960b07190f943f7c22056d82ffb050bef9f
Giuseppe Caruso <carusog@baxenergy.com>
2018-04-27 08:59:14 +0200

    [Lesson 02 - HTML] Add starting text

--- src-server/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { timeRoute } from './time/time-route';
-import { Express } from 'express';
-
-// register here any API controller
-export const routes: Array<(app: Express) => void> = [
-    timeRoute
-];
--- src-server/routes/time/time-route.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from "express-serve-static-core";
-import { Request, Response, NextFunction } from "express";
-
-export const timeRoute = function(app: Express) {
-    app.get('/api/time/now', function(request: Request, result: Response, next: NextFunction) {
-        result.setHeader('Content-Type', 'application/json');
-        result.send(JSON.stringify({ now: new Date().toISOString() }));
-    });
-};
