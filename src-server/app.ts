0b53a727f489247b42b0913e83d6a0204a40b6ef
Domenico Garozzo <garozzo@baxenergy.com>
2018-05-10 16:31:12 +0200

    Removed lesson2 folder from src-server

--- src-server/app.ts
+++ src-server/app.ts
@@ -1,8 +1,5 @@
 import * as express from 'express';
 import { routes } from './routes';
-import { first } from './lesson2/first';
-import { Vip } from './lesson2/second';
-import { GotCharacter } from './lesson2/third';
 
 export const app = express();
 
