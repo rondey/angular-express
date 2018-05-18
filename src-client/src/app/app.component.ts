ed29483e2962dd3eba7f837eda789de44523bc95
spatola <vincenzo.spatola@gmail.com>
2018-05-18 09:44:55 +0200

    Cleanup

--- src-client/src/app/app.component.ts
+++ src-client/src/app/app.component.ts
@@ -1,47 +1,14 @@
 import {
-  Component, OnChanges, OnInit, DoCheck, AfterContentInit,
-  AfterContentChecked, AfterViewInit, AfterViewChecked, SimpleChanges
+  Component
 } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { MatDialog } from '@angular/material';
-import { AboutComponent } from './about.component';
-import { GuestbookService } from './guestbook.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
+  // encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
-  public now: Date;
-
-  constructor(
-    client: HttpClient,
-    private guestbookService: GuestbookService,
-    private matDialog: MatDialog) {
-
-    client.get('/api/time/now').subscribe((result: { [now: string]: string }) => {
-      this.now = result.now ? new Date(result.now) : new Date();
-    }, () => {
-      this.now = new Date();
-    });
-  }
-
-  public openAbout(text?: string) {
-    this.matDialog.open(AboutComponent, {
-      data: text ? text : 'Made with love by nZo'
-    });
-  }
-
-  public openHelp() {
-    this.openAbout('The answer to any question is 42!');
-  }
-
-  public reload() {
-    console.log('Reloading application state');
-  }
-
-  public clear() {
-    this.guestbookService.clear();
-  }
+  constructor() {}
 }
--- src-client/src/app/app.module.ts
+++ src-client/src/app/app.module.ts
@@ -2,24 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { GuestbookComponent } from './guestbook.component';
 import { AppRoutingModule } from './app-routing.module';
-import {
-  MatToolbarModule,
-  MatIconModule,
-  MatInputModule,
-  MatButtonModule,
-  MatMenuModule,
-  MatDialogModule,
-  MatCardModule,
-} from '@angular/material';
-import { AboutComponent } from './about.component';
-import { GuestbookService } from './guestbook.service';
-import { GuestComponent } from './guest.component';
-import { SignComponent } from './sign.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 // Decorator
 // Definizione di una funzione utilizzata per "decorare" un altra funzione
@@ -31,27 +16,14 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   declarations: [
     AppComponent,
     GuestbookComponent,
-    PageNotFoundComponent,
-    AboutComponent,
-    GuestComponent,
-    SignComponent
+    PageNotFoundComponent
   ],
   // imports
   // usato per definire i moduli esterni da importare utilizzati per estendere le funzionalità del modulo corrente
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatInputModule,
-    MatMenuModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatCardModule
+    AppRoutingModule
   ],
   // exports
   // usato per definire tutti i Compoents/Directives/Pipes/Modules da rendere disponibili ad altri moduli
@@ -60,12 +32,10 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   // providers
   // usato per definire tutti i services che questo modulo deve istanziare e rendere disponibili nell'app
   providers: [
-    GuestbookService
   ],
   // entryComponents
   // usato per definire i Components da poter istanziare via codice
   entryComponents: [
-    AboutComponent
   ],
   // bootstraps
   // usato per definire i componenti da avviare. Solo un rootmodule deve avere la definizione di bootstrap
